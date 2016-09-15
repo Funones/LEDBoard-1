ac56016b0e839959aec6accbc2b9d332d995e6a8
garc9908 <alagarcia@csumb.edu>
2016-09-15 18:03:56 +0000

    added Letter O

--- ledLetters.php
+++ ledLetters.php
@@ -108,7 +108,11 @@ function drawLetter($letter, $color) {
 					}*/
 					break;
 				case "O" :
-					if (1) {
+					if (
+						(($i == 0 || $i == 7) && ($j > 0 || $j < 7)) ||
+						(($i > 0 || $i < 7) && ($j == 0 || $j == 7)) 
+						
+					) {
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
 					}
